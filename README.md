025806133f5636900b57379cea66425be737a5cd
keszotrab <39887624+keszotrab@users.noreply.github.com>
2026-05-06 08:19:39 +0200

    map fixes

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
-# LithiaMapProject 
-A simple project for a map/wiki app based on my game's world.
+# LithiaMapProject
+A simple project for a map/wiki app based on my WIP game's world.
 
-# WORK IN PROGRESS
+# !!!WORK IN PROGRESS!!!
 
-#Todo:
--fix window referenceError
--fix the rendering when scrolled too much
--fix scrolling zoom to zoom towards the mouse cursor placement. 
--add Tile Cache, lazy loading
--place map into a nicer component/divs
--add UI, window border, little div/box for info about locations. 
--add Nodes on map, that when clicked load the info about the node. 
--Beautify the info window a bit. Animation with a scroll opening/closing when tapped on top of the window or node. 
+## Funkcje
+- [x] Wyświetlanie mapy i scrollowanie jej. 
 
--Make a separete info/wiki site.
--Add a relation graph to the site with nodes connected between each other showing relations between them, based on chosen filter
+
+## Todos
+
+- [] fix window referenceError
+- [x] fix the rendering when scrolled too much
+- [x] fix scrolling zoom to zoom towards the mouse cursor placement. 
+- [] add UI, window border, little div/box for info about 
+- [] add Nodes on map, that when clicked load the info about the node. 
+- [] add Tile Cache, lazy loading
+- [] place map into a nicer component/divs
+locations. 
+- [] Beautify the info window a bit. Animation with a scroll opening/closing when tapped on top of the window or node. 
+
+
+## Further plans roadmap
+- [] Make a separete info/wiki site.
+- [] Add a relation graph to the site with nodes connected between each other showing relations between them, based on chosen filter
--- src/map/map.component.ts
+++ src/map/map.component.ts
@@ -1,26 +1,34 @@
 import { Component, HostListener } from '@angular/core';
-import { TILE_IMG_FORMAT, TILE_NAME, TILE_SIZE_HEIGHT, TILE_SIZE_WIDTH, TILES_PER_ROW, WORLD_HEIGHT, WORLD_WIDTH } from './map.constants';
+import {
+  TILE_IMG_FORMAT,
+  TILE_NAME,
+  TILE_SIZE_HEIGHT,
+  TILE_SIZE_WIDTH,
+  TILES_PER_ROW,
+  WORLD_HEIGHT,
+  WORLD_WIDTH,
+} from './map.constants';
 import { Tile } from './tile.model';
+import { ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss'],
-  standalone:true
+  standalone: true,
 })
 export class MapComponent {
   camera = { x: 0, y: 0, zoom: 1 };
   visibleTiles: Tile[] = [];
   private dragging = false;
   private lastMouse = { x: 0, y: 0 };
-  
 
   ///////
 
   get worldTransform(): string {
     return `
-      translate(${-this.camera.x}px, ${-this.camera.y}px)
       scale(${this.camera.zoom})
+      translate(${-this.camera.x}px, ${-this.camera.y}px)
     `;
   }
 
@@ -54,26 +62,39 @@ export class MapComponent {
   onWheel(event: WheelEvent) {
     event.preventDefault();
 
-    const zoomDelta = event.deltaY > 0 ? 0.9 : 1.1;
-    this.camera.zoom *= zoomDelta;
+    const rect = (event.target as HTMLElement).getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
 
-    this.camera.zoom = Math.min(2.5, Math.max(0.5, this.camera.zoom));
+    const worldX = this.camera.x + mouseX / this.camera.zoom;
+    const worldY = this.camera.y + mouseY / this.camera.zoom;
+
+    const zoomDelta = event.deltaY > 0 ? 0.9 : 1.1; // obvious numbers? but probably add variables for that anyway (magic numbere)
+
+    this.camera.zoom = Math.min(2.5, Math.max(0.5, this.camera.zoom * zoomDelta)); // add variables from map constraints (magic numbers are here too)
+
+    this.camera.x = worldX - mouseX / this.camera.zoom; // this is a little wacky, tweak the numbers later
+    this.camera.y = worldY - mouseY / this.camera.zoom;
 
     this.clampCamera();
     this.updateVisibleTiles();
   }
-
+  //technically bug is fixed but It's not perfectly loading the Tiles
+  //Not a biggie, but have to check it again
   updateVisibleTiles() {
-    const startCol = Math.floor(this.camera.x / TILE_SIZE_WIDTH);
-    const startRow = Math.floor(this.camera.y / TILE_SIZE_HEIGHT);
+    const viewWidth = window.innerWidth / this.camera.zoom;
+    const viewHeight = window.innerHeight / this.camera.zoom;
 
-    const cols = Math.ceil(window.innerWidth / (TILE_SIZE_WIDTH * this.camera.zoom)) + 2; //1 or 2 idk
-    const rows = Math.ceil(window.innerHeight / (TILE_SIZE_HEIGHT * this.camera.zoom)) + 2;
+    const startCol = Math.floor(this.camera.x / TILE_SIZE_WIDTH) - 1;
+    const endCol = Math.ceil((this.camera.x + viewWidth) / TILE_SIZE_WIDTH) + 1;
 
-    const tiles = [];
+    const startRow = Math.floor(this.camera.y / TILE_SIZE_HEIGHT) - 1;
+    const endRow = Math.ceil((this.camera.y + viewHeight) / TILE_SIZE_HEIGHT) + 1;
 
-    for (let r = startRow; r < startRow + rows; r++) {
-      for (let c = startCol; c < startCol + cols; c++) {
+    const tiles: Tile[] = [];
+
+    for (let r = startRow; r <= endRow; r++) {
+      for (let c = startCol; c <= endCol; c++) {
         if (
           c < 0 ||
           r < 0 ||
